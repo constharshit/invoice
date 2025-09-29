6e7d34aad30fa2f72ec16c94c579013d20cbaea8
constharshit <tennisdude.vja@gmail.com>
2025-09-28 20:36:16 -0700

    App router fundamentals completed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Invoice App',
+    default: 'Invoice App',
+  },
+  description: 'Invoice App built using NextJS',
+  metadataBase: new URL('https://invoice-livid-seven.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
